59e92a86a3477b49dd9305d8699d2d5ba0546592
Fábio Galdino <fabio.galdino989@gmail.com>
2025-11-23 04:46:33 -0300

    Atualizando quantidade mínima de artigos para realizar briefing

--- config_base.py
+++ config_base.py
@@ -76,7 +76,7 @@ N_CLUSTERS = 10  # Example, adjust as needed
 MIN_INITIAL_FILTER_SCORE = 3
 
 # Minimum number of articles required to attempt clustering/briefing
-MIN_ARTICLES_FOR_BRIEFING = 5
+MIN_ARTICLES_FOR_BRIEFING = 20
 
 ARTICLES_PER_PAGE = 15
 
